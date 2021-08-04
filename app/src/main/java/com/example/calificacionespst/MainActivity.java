a384db802149184a9bc49ba1f910f1253470b31c
vicemora <vicemora@espol.edu.ec>
2021-08-03 21:58:58 -0500

    sds

--- app/src/main/java/com/example/calificacionespst/MainActivity.java
+++ app/src/main/java/com/example/calificacionespst/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.calificacionespst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Manejador para el evento onClick para el botón de ingresar como profesor
+     * @param v
+     */
+    public void profesor(View v){
+
+        Intent i = new Intent(this, MainActivity2.class ); // Permite enlazar con el activity para profesores
+        startActivity(i); //inicia el activity
+    }
+
+    /**
+     * Manejador para el evento onclick del botón para ingresar como estudiante
+     * @param v
+     */
+    public void estudiante(View v){
+        Intent i = new Intent(this, MainActivity3.class ); // Permite enlazar con el activity para profesores
+        startActivity(i); //inicia el activity
     }
 
 
