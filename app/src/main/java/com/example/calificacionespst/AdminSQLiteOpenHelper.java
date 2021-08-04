0dfa5547ec57eb8bff289be5cfebeaebda35e70f
vicemora <vicemora@espol.edu.ec>
2021-08-03 21:33:01 -0500

    primer commit

---
+++ app/src/main/java/com/example/calificacionespst/AdminSQLiteOpenHelper.java
@@ -0,0 +1,20 @@
+package com.example.calificacionespst;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+    public AdminSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table calificaciones(matricula int primary key,nombre text,calificacion real)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+}
